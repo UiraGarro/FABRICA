7c88fe7961fafbface92c3024c267aacf8f5e667
UiraGarro <extadexavier@gmail.com>
2025-02-24 12:43:26 -0300

    Update desafio da fabrica.py

--- desafio da fabrica.py
+++ desafio da fabrica.py
@@ -37,51 +37,39 @@ print("2. Subtração")
 print("3. Multiplicação")
 print("4. Divisão")
 print("5. Mostrar Histórico")
-   
+print("6. Limpar Histórico")
+histórico = []
+
 while True:
-    escolha = input("Digite sua escolha (1/2/3/4/5): ")
+    escolha = input("Digite sua escolha (1/2/3/4/5/6): ")
     
-    if escolha in ['1', '2', '3', '4', '5']:
-        num1 = float(input("Digite seu primeiro número: "))
-        num2 = float(input("Digite seu segundo número: "))
-             
-        
-        if escolha == '1':
-            print(f"{num1} + {num2} = {Calculadora.soma(num1, num2)}")   
-                  
-        elif escolha == '2':
-            print(f"{num1} - {num2} = {Calculadora.subtração(num1, num2)}")
+    if escolha in ['1', '2', '3', '4', '5', '6']:
+        if escolha == '5':
+            print(histórico)
+        elif escolha == '6': histórico.clear()
+        else:
+            num1 = float(input("Digite seu primeiro número: "))
+            num2 = float(input("Digite seu segundo número: "))
+                
             
-        elif escolha == '3':
-            print(f"{num1} * {num2} = {Calculadora.multiplicação(num1, num2)}")
-            
-        elif escolha == '4':
-            print(f"{num1} / {num2} = {Calculadora.divisão(num1, num2)}")
+            if escolha == '1':
+                print(f"{num1} + {num2} = {Calculadora.soma(num1, num2)}")   
+                histórico.append(Calculadora.soma(num1, num2))
+            elif escolha == '2':
+                print(f"{num1} - {num2} = {Calculadora.subtração(num1, num2)}")
+                histórico.append(Calculadora.subtração(num1, num2))
+                
+            elif escolha == '3':
+                print(f"{num1} * {num2} = {Calculadora.multiplicação(num1, num2)}")
+                histórico.append(Calculadora.multiplicação(num1, num2))
+                
+            elif escolha == '4':
+                print(f"{num1} / {num2} = {Calculadora.divisão(num1, num2)}")
+                histórico.append(Calculadora.divisão(num1, num2))
+    
     
     proxima_operação = input("Deseja realizar outra operação? (s/n): ")
     if proxima_operação.lower() != 's':
             break
          
-    else:print("1. Soma  2. Subtração  3.Multiplicação  4.Divisão 5. Mostrar histórico")
-        
-        
-        #histórico = []
-        
-        #if escolha == '5':
-            #print(f"Histórico de contas: {histórico.append()}")
-        #resultado1 = {Calculadora.soma(num1, num2)}
-        #resultado2 = {Calculadora.subtração(num1, num2)}
-        #resultado3 = {Calculadora.multiplicação(num1, num2)}
-        #resultado4 = {Calculadora.divisão(num1, num2)}
-        
-       # if escolha == '1':
-        #    print(resultado1)
-        
-      #  elif escolha == '2':
-      #      print(resultado2)
-        
-      #  elif escolha == '3':
-      #      print(resultado3) 
-            
-     #   elif escolha == '4':
-     #       print(resultado4)
+    else:print("1. Soma  2. Subtração  3.Multiplicação  4.Divisão 5. Mostrar histórico 6.Limpar histórico")
